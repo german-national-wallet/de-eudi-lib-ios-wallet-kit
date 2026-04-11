c353a75d8cae2fd8234284708fc63339d074e50f
Pankaj Sachdeva <pankaj.sachdeva@eudi.sprind.org>
2026-04-11 15:25:11 +0200

    WD-2188: Use attestation proof type in place of jwt to support key attestation

--- Sources/EudiWalletKit/Compatibility/WalletApp/OpenId4VciService+WalletAppInternalCompatibility.swift
+++ Sources/EudiWalletKit/Compatibility/WalletApp/OpenId4VciService+WalletAppInternalCompatibility.swift
@@ -22,4 +22,16 @@ extension OpenId4VCIService {
 			issuer: config.clientId
 		)
 	}
+
+	func getKeyAttestationJWTForWalletAppCompatibility(_ publicKeys: [ECPublicKey], nonce: String?) async throws -> KeyAttestationJWT {
+		guard let additionalOptions = issueReq.keyOptions?.additionalOptions else {
+			throw PresentationSession.makeError(str: "additionalOptions not found")
+		}
+		let provider = self.config.keyAttestationsConfig!.walletAttestationsProvider as? any WalletAttestationsProviderForWalletAppCompatibility
+		guard let docType = String(data: additionalOptions, encoding: .utf8),
+			  let wte = try await provider?.getKeysAttestation(docType: docType) else {
+			throw PresentationSession.makeError(str: "wte not found")
+		}
+		return try .init(jws: .init(compactSerialization: wte))
+	}
 }
